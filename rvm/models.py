19da8c49c79be945a6a9208d364eec443e714630
jcscale <jcescala700@yahoo.com>
2022-03-16 00:09:19 +0800

    added withdraw table

--- rvm/models.py
+++ rvm/models.py
@@ -15,26 +15,37 @@ class User(AbstractUser):
 class Account(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     mobile_number = PhoneNumberField()
+    pin_number = models.CharField(max_length=4)
     previous_number_of_bottles = models.IntegerField(blank=True, null=True)
     previous_credits_earned = models.DecimalField(
         decimal_places=2, max_digits=15, blank=True, null=True)
     total_number_of_bottles = models.IntegerField(default=0)
     total_credits_earned = models.DecimalField(
         max_digits=12, decimal_places=2, default=0.00)
+    slug = AutoSlugField(populate_from=['mobile_number'])
 
     def __str__(self):
         return str(self.user)
 
 
-class Transaction(models.Model):
+class Deposit(models.Model):
     mobile_number = PhoneNumberField()
     credits_earned = models.DecimalField(
         decimal_places=2, max_digits=15, blank=True, null=True)
     number_of_bottles = models.IntegerField(blank=True, null=True)
-    date = models.DateField(default=datetime.date.today, blank=True, null=True)
+    date = models.DateField(default=datetime.date.today)
 
     class Meta:
         ordering = ['-date']
 
     def __str__(self):
         return str(self.mobile_number)
+
+
+class Withdraw(models.Model):
+    mobile_number = PhoneNumberField()
+    amount = models.IntegerField(default=0)
+    date = models.DateField(default=datetime.date.today)
+
+    def __str__(self):
+        return str(self.user)
